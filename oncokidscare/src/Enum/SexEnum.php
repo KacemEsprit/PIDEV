51081fde26ee6a0ebb1a766bb98a7180c36e3ebb
KacemEsprit <Kacem.BENBRAHIM@esprit.tn>
2025-02-08 16:47:51 +0100

    3rd commit

---
+++ oncokidscare/src/Enum/SexEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enum;
+
+enum SexEnum: string
+{
+    case MALE = 'male';
+    case FEMALE = 'female';
+
+}
---
+++ oncokidscare/src/Enum/SpecialiteEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum SpecialiteEnum: string
+{
+    case MALE = 'role1';
+    case Kacem = 'role2';
+    case Yazid = 'role3';
+
+
+}
